38f65702c334a8195faf06f2d7e8c671ad0e010a
David Camacho <david.camacho@globant.com>
2023-07-08 02:04:33 -0500

    feat: add two sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-export default function Home() {
+
+export default function Me() {
   return (
-    <main >
-      <div>
-        <code>David Camacho...</code>
-      </div>
-    </main>
+    <code>
+      Web UI Developer with +7 years of experience providing IT solutions for private and public
+      organizations using JavaScript, ReactJS, React Native, NodeJS, AWS.Aiming to contribute to building world-class
+      software solutions along with highly performant and dynamic teams. 2+ years leading teams under Scrum methodology. Certified in Scrum Master (International Scrum Institute) and Cloud Practitioner (AWS).
+      I have also worked with OTT Applications for a few years, I have participated in features oriented to TTS (Text to Speech) and Voice Speech Recognition for LG devices and different features like Magic Remote events and others.
+      Although this is a small summary, you can find more information in the Experience section...
+    </code>
   )
 }
